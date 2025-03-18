457d1b8304665c4b922b0e6d79f87d4de1cfc7f4
Ronnie DuPree Jr <ronniedupree@gmail.com>
2025-03-18 01:44:52 -0700

    html/css calculator draft

---
+++ calc.js
@@ -0,0 +1,30 @@
+let op = "";
+let a = 0;
+let b = 0;
+
+const add = function(a, b) {
+    return a + b;
+}
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+const divide = function(a, b) {
+    return a / b;
+}
+
+const operate = function(op, a, b) {
+    switch (op) {
+        case "+": return add(a, b);
+        case "-": return subtract(a, b);
+        case "*": return multiply(a, b);
+        case "/": return divide(a, b);
+        default:
+            console.log("Missing operation");
+    }
+}
